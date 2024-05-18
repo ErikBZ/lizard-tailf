7c816627f9df2c2bcf57f02751e320f6291d2261
Erik Zapata <zapatabrandon@gmai.com>
2024-05-18 17:53:41 -0400

    Dumb implementation of printing last 10 lines of file

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,66 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+const process = std.process;
+
+const DEFAULT_LINES = 10;
 
 pub fn main() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    var file = try std.fs.cwd().openFile("foo.txt", .{});
-    defer file.close();
+    var args = process.args();
+    _ = args.skip();
+
+    const path = args.next();
+
+    if (path) |good_path| {
+        try dumb_tail(good_path, allocator);
+    } else {
+        print_usage();
+    }
+}
+
+fn print_usage() void {
+    std.debug.print("Usage: lizard-tail [OPTION]... [FILE]...\n", .{});
+    std.debug.print("Print the last 10 lines of each FILE to standard output\n", .{});
+}
+
+fn dumb_tail(path: []const u8, allocator: Allocator) !void {
+    const file = try std.fs.cwd().openFile(path, .{});
+
+    var lines = ArrayList([]const u8).init(allocator);
+    defer {
+        file.close();
+        lines.deinit();
+    }
 
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
     var buf: [1024]u8 = undefined;
+
+    // need to copy line into a new []const u8
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        // this line messed up i bet
-        try stdout.print("{s}\n", .{line});
+        const stuff = try allocator.alloc(u8, line.len);
+        std.mem.copyForwards(u8, stuff, line);
+        try lines.append(stuff);
     }
 
-    try stdout.print("Hello World\n", .{});
+    const end = lines.items.len;
+    const start = if (end > DEFAULT_LINES) end - DEFAULT_LINES else 0;
 
-    // clean up
-    try bw.flush();
+    for (lines.items[start..end]) |line| {
+        std.debug.print("{s}\n", .{line});
+    }
+
+    // just 2 dots for the index stuff
+    for (lines.items) |line| {
+        allocator.free(line);
+    }
 }
 
-// TODO create function for opening a file and reading the whole file.
-// then count the newlines and split across em
+test "Test for memory leaks in dumb_tail" {
+    const test_allocator = std.testing.allocator;
+    try dumb_tail("foo.txt", test_allocator);
+}
